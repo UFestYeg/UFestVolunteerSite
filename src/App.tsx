a2228454948799d82623765485bcf8ec520cfb6e
Gillian Pierce <gmpierce@ualberta.ca>
2020-07-27 00:33:31 -0600

    initial profile page layout idea and styling/theming things

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,15 @@
 import { CssBaseline } from "@material-ui/core";
 
-import {
-    createMuiTheme,
-    responsiveFontSizes,
-    ThemeProvider,
-} from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/core/styles";
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import BaseRouter from "./routes";
 import { auth as actions } from "./store/actions";
+import { theme } from "./styles";
 
 const App: React.FC = () => {
-    let theme = createMuiTheme();
-    theme = responsiveFontSizes(theme);
     const dispatch = useDispatch();
 
     useEffect(() => {
---
+++ src/containers/NavDrawer/NavDrawer.tsx
@@ -0,0 +1,119 @@
+import React from "react";
+import {
+    Drawer,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
+} from "@material-ui/core";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import SettingsIcon from "@material-ui/icons/Settings";
+import CalendarTodayIcon from "@material-ui/icons/CalendarToday";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import { makeStyles } from "@material-ui/core/styles";
+import { NavLink } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { auth as actions } from "../../store/actions";
+
+const useStyles = makeStyles((theme) => ({
+    active: {
+        background: theme.palette.primary.main,
+        color: "white",
+        "& svg": {
+            fill: "white",
+            "& path": {
+                fill: "white!important",
+            },
+        },
+        "&:hover": {
+            background: theme.palette.primary.main,
+        },
+    },
+    inactive: {
+        color: "rgba(0, 0, 0, 0.54)",
+    },
+}));
+
+interface NavListItemProps {
+    text: string;
+    icon: JSX.Element;
+    to: string;
+    activeClassName: string;
+    onClose: Function;
+}
+
+const NavListItem = ({
+    text,
+    icon,
+    to,
+    activeClassName,
+    onClose,
+}: NavListItemProps) => {
+    return (
+        <ListItem
+            button
+            component={NavLink}
+            exact={true}
+            to={to}
+            activeClassName={activeClassName}
+            onClick={() => onClose(false)}
+        >
+            <ListItemIcon>{icon}</ListItemIcon>
+            <ListItemText primary={text} />
+        </ListItem>
+    );
+};
+
+interface NavDrawerProps {
+    open: boolean;
+    onCloseFunc: Function;
+}
+
+const NavDrawer: React.FC<NavDrawerProps> = ({
+    open,
+    onCloseFunc,
+}: NavDrawerProps) => {
+    const styles = useStyles();
+    const dispatch = useDispatch();
+    const handleLogout = () => {
+        onCloseFunc(false);
+        // logout({ returnTo: window.location.origin });
+        dispatch(actions.logout());
+    };
+    return (
+        <Drawer open={open} onClose={() => onCloseFunc(false)}>
+            <List className={styles.inactive}>
+                <NavListItem
+                    to="/events"
+                    icon={<CalendarTodayIcon />}
+                    text="Schedule"
+                    activeClassName={styles.active}
+                    onClose={onCloseFunc}
+                />
+                <NavListItem
+                    to="/profile"
+                    icon={<AccountCircleIcon />}
+                    text="Profile"
+                    activeClassName={styles.active}
+                    onClose={onCloseFunc}
+                />
+                <NavListItem
+                    to="/settings"
+                    icon={<SettingsIcon />}
+                    text="Settings"
+                    activeClassName={styles.active}
+                    onClose={onCloseFunc}
+                />
+                <NavListItem
+                    to=""
+                    icon={<ExitToAppIcon />}
+                    text="Logout"
+                    activeClassName={styles.active}
+                    onClose={handleLogout}
+                />
+            </List>
+        </Drawer>
+    );
+};
+
+export default NavDrawer;
