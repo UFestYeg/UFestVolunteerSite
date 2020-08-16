07900bb66080737864b4b5aa3fbf7c82147b453b
LukeSlev <lslevins@ualberta.ca>
2020-08-16 14:07:14 -0600

    temp commit

--- backend/settings.py
+++ backend/settings.py
@@ -184,6 +184,11 @@ REST_AUTH_REGISTER_SERIALIZERS = {
 # emails
 
 # change this in prod: https://simpleit.rocks/python/django/adding-email-to-django-the-easiest-way/
-EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+# EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
-DEFAULT_FROM_EMAIL = "ufest.ca"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "ufestmarketing@gmail.com"
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_PASSWORD")
