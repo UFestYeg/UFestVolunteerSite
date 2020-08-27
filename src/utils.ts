fa05ea21bd116a207f7bdd04c60338a9d87b84e4
LukeSlev <lslevins@ualberta.ca>
2020-08-27 11:59:43 -0600

    add sorting to array comparison

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,7 @@
-export const compareArrays = (a: any[], b: any[]) => {
+export const compareArrays = (a: any[], b: any[], sort: boolean = true) => {
+    if (sort) {
+        a.sort();
+        b.sort();
+    }
     return JSON.stringify(a) === JSON.stringify(b);
 };
