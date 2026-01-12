0507b039cefa6eada0a0f96f6a2ba545d7a2b29b
Luke Slevinsky <luke.slev@gmail.com>
2026-01-12 12:20:51 -0800

    Revert "Extract filled_positions annotation logic into helper method"
    This reverts commit e92f057aa87202674c96d8e8cd17fec0b642307e.

--- volunteer_categories/api/views.py
+++ volunteer_categories/api/views.py
@@ -29,13 +29,6 @@ class VolunteerCategoryViewSet(viewsets.ModelViewSet):
 
     serializer_class = VolunteerCategorySerializer
 
-    @staticmethod
-    def _get_roles_with_filled_positions():
-        """Helper method to get roles queryset annotated with filled_positions count."""
-        return Role.roles.annotate(
-            filled_positions=Count('requests', filter=Q(requests__status=Request.ACCEPTED))
-        )
-
     def get_queryset(self):
         """
         Optionally restricts the returned categories to a given user,
@@ -46,7 +39,9 @@ class VolunteerCategoryViewSet(viewsets.ModelViewSet):
         # Only add prefetch_related if not calling with_requests action
         # The with_requests action sets up its own optimized prefetch operations
         if self.action != 'with_requests':
-            roles_qs = self._get_roles_with_filled_positions()
+            roles_qs = Role.roles.annotate(
+                filled_positions=Count('requests', filter=Q(requests__status=Request.ACCEPTED))
+            )
             queryset = queryset.select_related('category_type').prefetch_related(
                 Prefetch('roles', queryset=roles_qs)
             )
